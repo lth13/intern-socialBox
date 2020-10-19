626dda8d0c3682f917946298e1862ad4d519760b
lth13 <lth.hnue@gmail.com>
2020-10-20 03:25:50 +0700

    add todo list

---
+++ todoList/vue.js
@@ -0,0 +1,72 @@
+var vm = new Vue({
+    el: '#container',
+    data:{
+        todo: '',
+        id: 3,
+        listTodo: JSON.parse(localStorage.getItem('todo')),
+        beforeEdit: '',
+        todoList: []
+    },
+    directives: {
+        focus: {
+            inserted: function (el) {
+                el.focus()
+            }
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.todo.trim().length == 0){
+                return
+            }
+            this.todoList.push({
+                'id':this.id,
+                'content': this.todo,
+                'complete': false,
+                'editing': false
+            })
+            this.id++
+            this.todo = ''
+            localStorage.setItem('todo', JSON.stringify(this.todoList))
+            this.listTodo = JSON.parse(localStorage.getItem('todo'))
+        },
+        editItem(item){
+            item.editing=true
+            this.beforeEdit = item.content
+        },
+        doneEdit(item){
+            if(item.content.trim() == ''){
+                item.content= this.beforeEdit
+            }
+            item.editing=false
+            this.todoList.forEach(function (todo) {
+                if(todo.id == item.id) {
+                    todo.content = item.content
+                }
+            })
+        },
+        removeItem(index){
+            this.todoList.splice(index, 1)
+            localStorage.setItem('todo', JSON.stringify(this.todoList))
+            this.listTodo = JSON.parse(localStorage.getItem('todo'))
+        },
+        enter(item){
+            this.listTodo.forEach(function (x) {
+                if(x.id == item.id){
+                    !x.complete
+                }
+            })
+            localStorage.setItem('todo', JSON.stringify(this.listTodo))
+            this.listTodo = JSON.parse(localStorage.getItem('todo'))
+        },
+        render(item){
+            this.listTodo.forEach(function (x) {
+                if(x.id == item.id){
+                    x.content = item.content
+                }
+            })
+            localStorage.setItem('todo', JSON.stringify(this.listTodo))
+            this.listTodo = JSON.parse(localStorage.getItem('todo'))
+        }
+    }
+})
